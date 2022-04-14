03b8c10a1747b09ddad150bea36a4d69305398fa
charlie7002 <chaced@orange.fr>
2022-04-14 15:32:00 +0200

    checkout lasttrip ticketfound front

--- models/users.js
+++ models/users.js
@@ -2,44 +2,44 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 
 var userSchema = mongoose.Schema({
-    Name: String,
-    FirstName: String,
-    email: String,
-    password: String,
-})
-
-userSchema.pre("save", function(next) {
-    const user = this;
-
-    if (this.isModified("password") || this.isNew) {
-        bcrypt.genSalt(10, function(saltError, salt) {
-            if (saltError) {
-                return next(saltError)
-            } else {
-                bcrypt.hash(user.password, salt, function(hashError, hash) {
-                    if (hashError) {
-                        return next(hashError)
-                    }
-                    user.password = hash
-                    next()
-                })
-            }
-        })
-    } else {
-        return next()
-    }
+	Name: String,
+	FirstName: String,
+	email: String,
+	password: String,
 });
 
-userSchema.methods.comparePassword = function(password, callback) {
-    bcrypt.compare(password, this.password, function(error, isMatch) {
-        if (error) {
-            return callback(error)
-        } else {
-            callback(null, isMatch)
-        }
-    })
+userSchema.pre('save', function (next) {
+	const user = this;
+
+	if (this.isModified('password') || this.isNew) {
+		bcrypt.genSalt(10, function (saltError, salt) {
+			if (saltError) {
+				return next(saltError);
+			} else {
+				bcrypt.hash(user.password, salt, function (hashError, hash) {
+					if (hashError) {
+						return next(hashError);
+					}
+					user.password = hash;
+					next();
+				});
+			}
+		});
+	} else {
+		return next();
+	}
+});
+
+userSchema.methods.comparePassword = function (password, callback) {
+	bcrypt.compare(password, this.password, function (error, isMatch) {
+		if (error) {
+			return callback(error);
+		} else {
+			callback(null, isMatch);
+		}
+	});
 };
 
-var userModel = mongoose.model('users', userSchema)
+var userModel = mongoose.model('users', userSchema);
 
 module.exports = userModel;
