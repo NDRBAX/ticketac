ac50d48649a0b83dd3173512e7a1e2a605299316
NDRBAX <andre.bacaliu@icloud.com>
2022-04-15 11:57:48 +0200

    Minor corrections

--- routes/index.js
+++ routes/index.js
@@ -8,89 +8,96 @@ var userModel = require('../models/users');
 var ticket = [];
 
 function upperFirst(string) {
-	return string.charAt(0).toUpperCase() + string.slice(1).toLocaleLowerCase();
+    return string.charAt(0).toUpperCase() + string.slice(1).toLocaleLowerCase();
 }
 
 /* GET LOGIN PAGE */
-router.get('/', function (req, res, next) {
-	res.render('login');
+router.get('/', function(req, res, next) {
+    res.render('login');
 });
 
 /* GET HOMEPAGE */
-router.get('/homepage', async function (req, res, next) {
-	if (req.session.user == null) {
-		res.redirect('/');
-	} else {
-		var lasttrips = await lasttrip.find();
-		res.render('homepage', { lasttrips });
-	}
+router.get('/homepage', async function(req, res, next) {
+    if (req.session.user == null) {
+        res.redirect('/');
+    } else {
+        var lasttrips = await lasttrip.find();
+        res.render('homepage', { lasttrips });
+    }
 });
 
 /* GET ERRORS */
-router.get('/notrain', function (req, res, next) {
-	res.render('errors');
+router.get('/notrain', function(req, res, next) {
+    res.render('errors');
 });
 
 /* GET TICKET-FOUND */
-router.post('/ticketfound', async function (req, res, next) {
-	var tickets = await journey.find({
-		departure: upperFirst(req.body.departure),
-		arrival: upperFirst(req.body.arrival),
-		date: req.body.date,
-	});
-	if (tickets.length == 0) {
-		res.render('notrain');
-	} else {
-		res.render('ticketfound', { tickets });
-	}
+router.post('/ticketfound', async function(req, res, next) {
+    var tickets = await journey.find({
+        departure: upperFirst(req.body.departure),
+        arrival: upperFirst(req.body.arrival),
+        date: req.body.date,
+    });
+    if (tickets.length == 0) {
+        res.render('notrain');
+    } else {
+        res.render('ticketfound', { tickets });
+    }
+});
+
+/* ADD TICKET */
+router.get('add-ticket', async function(req, res, next) {
+    const ticketId = req.query.ticketId;
+    console.log(ticketId);
+    res.render('checkout', { ticket });
 });
 
 /* GET CHECKOUT */
-router.get('/checkout', async function (res, req, next) {
-	var status = false;
-	var userSession = req.session.user;
+router.get('/checkout', async function(req, res, next) {
+    var status = false;
+    var userSession = req.session.user;
 
-	for (var i = 0; i < ticket.length; i++) {
-		if (req.query.id == ticket[i].id) {
-			status = true;
-		}
-	}
-	if (status == false) {
-		ticket.push({
-			departure: req.query.departure,
-			arrival: req.query.arrival,
-			departureTime: req.query.departureTime,
-			date: req.query.date,
-			price: req.query.price,
-			id: req.query.id,
-			iduser: userSession.id,
-		});
-	}
-	res.render('checkout', { ticket });
+    for (var i = 0; i < ticket.length; i++) {
+        if (req.query.id == ticket[i].id) {
+            status = true;
+        }
+    }
+    if (status == false) {
+        ticket.push({
+            departure: req.query.departure,
+            arrival: req.query.arrival,
+            departureTime: req.query.departureTime,
+            date: req.query.date,
+            price: req.query.price,
+            id: req.query.id,
+            iduser: userSession.id,
+        });
+    }
+    res.render('checkout', { ticket });
 });
 
 /* GET LAST-TRIP */
-router.get('/lasttrip', async function (req, res, next) {
-	for (var i = 0; i < ticket.length; i++) {
-		var saveLastTrip = new lasttrip({
-			departure: ticket[i].departure,
-			arrival: ticket[i].arrival,
-			date: ticket[i].date,
-			departureTime: ticket[i].departureTime,
-			price: ticket[i].price,
-			id: ticket[i].id,
-			iduser: ticket[i].iduser,
-		});
-		await saveLastTrip.save();
-	}
+router.get('/lasttrip', async function(req, res, next) {
+    for (var i = 0; i < ticket.length; i++) {
+        var saveLastTrip = new lasttrip({
+            departure: ticket[i].departure,
+            arrival: ticket[i].arrival,
+            date: ticket[i].date,
+            departureTime: ticket[i].departureTime,
+            price: ticket[i].price,
+            id: ticket[i].id,
+            iduser: ticket[i].iduser,
+        });
+        await saveLastTrip.save();
+    }
 
-	var userSession = req.session.user;
+    var userSession = req.session.user;
 
-	let tickets = await lasttrip.find({ iduser: userSession.id });
-	for (var i = 0; i < ticket.length; i++) {
-		ticket.pop();
-	}
-	res.render('lasttrip', { tickets });
+    let tickets = await lasttrip.find({ iduser: userSession.id });
+    for (var i = 0; i < ticket.length; i++) {
+        ticket.pop();
+    }
+    res.render('lasttrip', { tickets });
 });
 
 module.exports = router;
